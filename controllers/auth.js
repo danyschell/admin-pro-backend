239d4aeb91720cfdeb226932e4b628ed1233b59b
Daniel Schell <schell.daniel@gmail.com>
2021-04-29 21:15:31 -0300

    Fin seccion 12 - Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const { validationResult } = require('express-validator');
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async(req, res = response) => {
@@ -51,6 +52,61 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSignIn = async(req, res = response) => {
+
+    const googleToken = req.body.token;
+
+    try{
+
+        const { name, email, picture } = await googleVerify(googleToken);
+
+        // verifico si ya existe ese email en nuestra DB
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if (!usuarioDB){
+            usuario = new Usuario({
+                nombre: name,
+                email, 
+                password: '@@@',
+                img: picture,
+                google: true
+            })
+
+        }else {
+            // ya existe porque se registró manualmente
+            usuario = usuarioDB;
+            usuario.google = true;
+            usuario.password = '@@@';
+
+        }
+
+        // Guardar en DB
+        await usuario.save();
+
+        // Generar token - JWT
+        const token = await generarJWT(usuarioDB.id);
+
+
+        res.json({
+            ok: true,
+            token
+        });
+
+    }
+    catch(error){
+
+        res.status(401).json({
+            ok: false,
+            msg: 'Error validando GoogleToken'
+        });
+
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
