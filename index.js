7b1320d5a3eb8b9b53ba9b452ff14b55518df007
Daniel Schell <schell.daniel@gmail.com>
2021-05-07 18:17:20 -0300

    SPA control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
@@ -27,6 +28,12 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/upload', require('./routes/uploads'));
+
+
+// si no entra en ninguna ruta de arriba...
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve( __dirname, 'public/index.html'));
+});
 
 //levantar servidor
 app.listen(process.env.PORT, () => {
